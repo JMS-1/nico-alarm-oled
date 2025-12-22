555a5dfa44beef6d18da0ecbb6e5f92ed42a1404
Dr. Jochen Manns <dev-2@psimarron.net>
2025-12-22 16:02:19 +0100

    Added web server.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
+#include <ESP8266mDNS.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
 #include <U8g2lib.h>
 #include <Wire.h>
-#include <ESP8266WiFi.h>
+
+const char *getHomePage(void);
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, 16);
+
+ESP8266WebServer server(80);
 
 WiFiClient client;
 
 bool blank = false;
+bool startMDNS = true;
 
 void writeText(const String &message)
 {
   u8g2.clearBuffer();
-  u8g2.drawStr(0, 26, message.c_str());
+  u8g2.drawStr(0, 21, message.c_str());
   u8g2.sendBuffer();
 
   blank = message.isEmpty();
@@ -20,11 +27,14 @@ void writeText(const String &message)
 
 void setup()
 {
+  Serial.begin(115200);
+
   pinMode(GPIO_AS_PIN_SOURCE, INPUT_PULLUP);
 
   u8g2.begin();
-  u8g2.setFont(u8g2_font_ncenB18_tr);
-  u8g2.enableUTF8Print();
+  u8g2.setFont(u8g2_font_ncenB10_tr);
+
+  WiFi.mode(WIFI_STA);
 
   auto ssid = WiFi.SSID();
   auto pass = WiFi.psk();
@@ -39,6 +49,21 @@ void connect()
 
   WiFi.disconnect();
   WiFi.beginWPSConfig();
+}
+
+void befehl(String cmd)
+{
+  if (server.hasArg("text"))
+  {
+    auto text = server.arg("text");
+
+    if (!text.isEmpty())
+      cmd = text;
+  }
+
+  writeText(cmd);
+
+  server.send(204);
 }
 
 void loop()
@@ -57,6 +82,56 @@ void loop()
     return;
   }
 
+  if (startMDNS)
+  {
+    startMDNS = false;
+
+    MDNS.begin("esp8266");
+
+    server.on("/RR", []
+              { befehl("RR"); });
+
+    server.on("/RG", []
+              { befehl("RG"); });
+
+    server.on("/RY", []
+              { befehl("RY"); });
+
+    server.on("/GR", []
+              { befehl("GR"); });
+
+    server.on("/GG", []
+              { befehl("GG"); });
+
+    server.on("/GY", []
+              { befehl("GY"); });
+
+    server.on("/YR", []
+              { befehl("YR"); });
+
+    server.on("/YG", []
+              { befehl("YG"); });
+
+    server.on("/YY", []
+              { befehl("YY"); });
+
+    server.on("/OFF", []
+              { befehl("OFF"); });
+
+    server.onNotFound([]
+                      { server.send(200, "text/html", getHomePage()); });
+
+    server.begin();
+  }
+
   if (!blank)
+  {
+    delay(1000);
+
     writeText("");
+  }
+
+  server.handleClient();
+
+  MDNS.update();
 }
